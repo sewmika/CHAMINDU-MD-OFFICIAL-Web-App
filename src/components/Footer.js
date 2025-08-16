47cc59c90bde7816259f0873490acb38bf1dad74
chamidu sewmika <chamidusewmika@gmail.com>
2025-08-16 11:31:37 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="bg-blue-600 text-white py-6 mt-12">
+      <div className="max-w-7xl mx-auto text-center">
+        <p>© 2025 CHAMINDU MD OFFICIAL. All rights reserved.</p>
+        <div className="flex justify-center space-x-4 mt-2">
+          <a href="#" className="hover:underline">Facebook</a>
+          <a href="#" className="hover:underline">Instagram</a>
+          <a href="#" className="hover:underline">YouTube</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
